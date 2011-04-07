3ade8739f5f1415cca91e8abc8052d48268ea7b7
Tato <tato.rodrigues@gmail.com>
2011-04-07 09:10:00 -0300

    #2 commit

--- app/controllers/twitt_controller.rb
+++ app/controllers/twitt_controller.rb
@@ -3,7 +3,7 @@ class TwittController < ApplicationController
   def home
 	search = Twitter::Search.new
 
-	@resp = search.from('t4to').result_type("recent").per_page(10).fetch
+	@resp = search.from('t4to').per_page(10).fetch
 
 	search.clear
   end
