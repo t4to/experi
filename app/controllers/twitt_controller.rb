921d18ba437484b2cfc63c603e4fae99f1ca1c58
Tato <tato.rodrigues@gmail.com>
2011-04-29 15:07:45 -0300

    ad

--- app/controllers/twitt_controller.rb
+++ app/controllers/twitt_controller.rb
@@ -1,9 +1,13 @@
 class TwittController < ApplicationController
   
   def home
+	screen_name = 't4to'
+	
+	@user = Twitter.user(screen_name)
+  
 	search = Twitter::Search.new
 
-	@resp = search.from('t4to').per_page(10).fetch
+	@resp = search.from(screen_name).per_page(10).fetch
 
 	search.clear
   end
